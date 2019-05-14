6f9193118643b2180a63083a68adb65be2c75cc2
LanYuan <launceviya@gmail.com>
2019-05-14 16:30:33 +0800

    v1.1,加入spring-cloud，简单配置eureka

--- src/main/java/org/lanren/idgenerator/IdGeneratorApplication.java
+++ src/main/java/org/lanren/idgenerator/IdGeneratorApplication.java
@@ -2,8 +2,12 @@ package org.lanren.idgenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
 public class IdGeneratorApplication {
 
     public static void main(String[] args) {
