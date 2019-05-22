c8160d45acc52619e0ed2bb097952533a452a094
LanYuan <launceviya@gmail.com>
2019-05-22 10:41:46 +0800

    id-generator eureka client config

--- src/main/java/org/lanren/idgenerator/IdGeneratorApplication.java
+++ src/main/java/org/lanren/idgenerator/IdGeneratorApplication.java
@@ -3,12 +3,21 @@ package org.lanren.idgenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
 public class IdGeneratorApplication {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(IdGeneratorApplication.class, args);
